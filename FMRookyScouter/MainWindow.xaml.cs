9a99c902ca0ed3bc5bc550a63c9b46e0cdfdca6a
junyoungpark10 <jaywapp16@gmail.com>
2023-02-22 16:33:30 +0900

    Update Datas

--- FMRookyScouter/MainWindow.xaml.cs
+++ FMRookyScouter/MainWindow.xaml.cs
@@ -1,18 +1,4 @@
-﻿using FMRookyScouter.Access;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace FMRookyScouter
 {
@@ -22,8 +8,8 @@ namespace FMRookyScouter
     public partial class MainWindow : Window
     {
         public MainWindow()
-        {
-            var access = new DBAccess();
+         {
+            DataContext = new MainWindowViewModel();
             InitializeComponent();
         }
     }
---
+++ FMRookyScouter/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FMRookyScouter.Access;
+using ReactiveUI;
+
+namespace FMRookyScouter
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+        public DBAccess Access { get; }
+
+        public MainWindowViewModel()
+        {
+            Access = new DBAccess();
+        }
+    }
+}
