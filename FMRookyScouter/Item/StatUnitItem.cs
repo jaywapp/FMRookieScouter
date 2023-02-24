8bf9a0900dc89504fa30981bcfced2d806683720
junyoungpark10 <jaywapp16@gmail.com>
2023-02-24 16:47:58 +0900

    add color level

--- FMRookyScouter/Item/StatUnitItem.cs
+++ FMRookyScouter/Item/StatUnitItem.cs
@@ -1,4 +1,6 @@
 ﻿using ReactiveUI;
+using System;
+using System.Windows.Media;
 
 namespace FMRookyScouter.Item
 {
@@ -6,5 +8,18 @@ namespace FMRookyScouter.Item
     {
         public string Name { get; set; }
         public int Value { get; set; }
+        public Color Color => GetColor();
+        
+        private Color GetColor()
+        {
+            return Colors.Yellow;
+            var color = Colors.Green;
+
+            var alpha = 0;
+            var interval = byte.MaxValue / 20;
+            alpha += (interval) * Value;
+
+            return Color.FromArgb((byte)alpha, color.R, color.G, color.B);
+        }
     }
 }
