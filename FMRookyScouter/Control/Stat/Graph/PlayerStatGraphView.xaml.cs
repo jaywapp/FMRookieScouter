d235d3d3028e7facff48502ca94dd4fa36db3606
unknown <jaywapp16@gmail.com>
2023-02-25 15:35:02 +0900

    add views about graph

---
+++ FMRookyScouter/Control/Stat/Graph/PlayerStatGraphView.xaml.cs
@@ -0,0 +1,76 @@
+﻿using FMRookyScouter.Control.Stat.Table;
+using FMRookyScouter.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FMRookyScouter.Control.Stat.Graph
+{
+    /// <summary>
+    /// PlayerStatGraphView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class PlayerStatGraphView : UserControl, INotifyPropertyChanged
+    {
+        #region Property Changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+
+        #region Internal Field
+        private UserControl _view;
+        #endregion
+
+        #region Properties
+        public UserControl View
+        {
+            get => _view;
+            set
+            {
+                _view = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public PlayerStatGraphView()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Functions
+        private void OnDataContextChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+        {
+            if (!(e.NewValue is Player player))
+                return;
+
+            View = GetView(player);
+        }
+
+        private static UserControl GetView(Player player)
+        {
+            if (player.Goalkeeping.IsGoalkeepper())
+                return new GoalkeepperPlayerStatGraphView() { DataContext = player };
+            else
+                return new FieldPlayerStatGraphView() { DataContext = player };
+        }
+        #endregion
+    }
+}
---
+++ FMRookyScouter/Control/Stat/Graph/StatGraphView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using FMRookyScouter.Interface;
+using FMRookyScouter.Item;
+using SkiaSharp;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FMRookyScouter.Control.Stat.Graph
+{
+    /// <summary>
+    /// StatGraphView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class StatGraphView : UserControl, INotifyPropertyChanged
+    {
+        #region Property Changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+
+        #region Constructor
+        public StatGraphView()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Functions
+        private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(e.NewValue is IStat stat))
+                return;
+
+            var items = stat.GetItems().ToList();
+        }
+        #endregion
+    }
+}
